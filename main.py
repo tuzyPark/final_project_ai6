970ca988e4e6fdd544497d6bf2c68559d1abd5bc
tuzyPark <105343510+tuzyPark@users.noreply.github.com>
2022-08-16 22:55:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def sentiment_predict(new_sentence):
     new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]','', new_sentence)
     new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
     new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
-    print(
     encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
     pad_new = pad_sequences(encoded, maxlen = MAX_LEN) # 패딩
     score = float(model.predict(pad_new)) # 예측
