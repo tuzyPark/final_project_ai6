dbba4cafddf5954060f0a63a82085a3d6b59f843
tuzyPark <105343510+tuzyPark@users.noreply.github.com>
2022-08-16 22:13:33 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,11 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from konlpy.tag import Okt
 
+import streamlit as st
+from bokeh.models.widgets import Button
+from bokeh.models import CustomJS
+from streamlit_bokeh_events import streamlit_bokeh_events
+
 
 
 MAX_LEN = 30
@@ -43,3 +48,30 @@ if btn_clicked:
   st.write(sentiment_predict(test_text))
   st.write(test_text)
 
+    
+    
+    
+    
+    
+    
+
+
+loc_button = Button(label="Get Location")
+loc_button.js_on_event("button_click", CustomJS(code="""
+    navigator.geolocation.getCurrentPosition(
+        (loc) => {
+            document.dispatchEvent(new CustomEvent("GET_LOCATION", {detail: {lat: loc.coords.latitude, lon: loc.coords.longitude}}))
+        }
+    )
+    """))
+result = streamlit_bokeh_events(
+    loc_button,
+    events="GET_LOCATION",
+    key="get_location",
+    refresh_on_update=False,
+    override_height=75,
+    debounce_time=0)
+
+if result:
+    if "GET_LOCATION" in result:
+        st.write(result.get("GET_LOCATION"))
