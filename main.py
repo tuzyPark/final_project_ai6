64ca3d362da96eb30907a5903b456723b298a471
tuzyPark <105343510+tuzyPark@users.noreply.github.com>
2022-08-16 20:12:48 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+from tensorflow import keras
+import numpy as np
+import tensorflow tf
+
+
+model_path = 'model/best_model_1.h5'
+model = keras.models.load_model(model_path)
+
+
+#def getPN(text):
+#  return model.predict([text])
+
+test_text = st.text_input('Movie title', 'Life of Brian')
+
+st.write(result_string)
+btn = st.button('결과 보기')
